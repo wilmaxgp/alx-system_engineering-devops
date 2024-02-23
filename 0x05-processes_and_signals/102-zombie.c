56eb24a160cef4baad51166e321fd3bb04fe94ab
wilmaxgp <dennisanietimfonwilliam005@gmail.com>
2024-02-23 06:44:50 -0800

    Add C program to create 5 zombie processes
    Implemented a C program 102-zombie.c that creates 5 zombie processes as per the requirements. The program uses fork() to create child processes, which immediately exit, becoming zombie processes. The parent process prints messages for each created zombie process along with its PID. Additionally, included a function infinite_while() to keep the program running. Tested successfully

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Creates an infinite loop
+ *
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	pid_t child_pid;
+	int i;
+	
+	for (i = 0; i < 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("fork");
+			return (1);
+		}
+		if (child_pid == 0)
+		{
+			exit(0);
+		}
+		else
+		{
+			printf("Zombie process created, PID: %d\n", child_pid);
+			sleep(1);
+		}
+	}
+	
+	infinite_while();
+	
+	return (0);
+}
