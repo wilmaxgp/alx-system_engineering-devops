7d72619b3f8e94c42c5fc15e738af0364b272910
Dennis William <dennisanietimfonwilliam005@gmail.com>
2024-08-14 04:11:13 +0100

    compliance to PEP8

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "my-user-agent"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code != 200:
         return 0
-    
+
     data = response.json().get('data', {})
     return data.get('subscribers', 0)
